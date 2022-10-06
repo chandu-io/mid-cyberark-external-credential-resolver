8e97b717dc89bf085e3c29bfde22f3976f2e8421
Chandrasekhar Thotakura <chandu-io@users.noreply.github.com>
2022-10-06 18:45:30 +0530

    Format codebase
    Signed-off-by: Chandrasekhar Thotakura <chandu-io@users.noreply.github.com>

--- src/com/snc/discovery/CredentialResolver.java
+++ src/com/snc/discovery/CredentialResolver.java
@@ -7,7 +7,6 @@ import java.util.regex.Pattern;
 import com.snc.automation_common.integration.creds.IExternalCredential;
 import com.snc.core_automation_common.logging.Logger;
 import com.snc.core_automation_common.logging.LoggerFactory;
-
 import javapasswordsdk.PSDKPassword;
 import javapasswordsdk.PSDKPasswordRequest;
 import javapasswordsdk.PasswordQueryFormat;
@@ -31,17 +30,25 @@ public class CredentialResolver implements IExternalCredential {
 	private static final String DEFAULT_SAFE_TIMEOUT = "10";
 	private static final String DEF_SAFE_CREDID_SPLIT = ":";
 
-	//Load below parameters from MID config parameters.
-	private String safeFolder = "";		// The Safe folder to use as specified in the MID config.xml file (must match folder name in CyberArk)
-	private String safeName = "";		// The Safe name to use as specified in the MID config.xml file (must match safe name in CyberArk)
-	private String safeAppID = ""; 		// The App-ID used when connecting to CyberArk (can be overridden in the config.xml file)
-	private String safeTimeout = "";	// The vault (server) response timeout in seconds to use as specified in the MID config.xml file
+	// Load below parameters from MID config parameters.
 
-	private String includeDomain = ""; 
+	// The Safe folder to use as specified in the MID config.xml file (must match folder name in CyberArk)
+	private String safeFolder = "";
+
+	// The Safe name to use as specified in the MID config.xml file (must match safe name in CyberArk)
+	private String safeName = "";
+
+	// The App-ID used when connecting to CyberArk (can be overridden in the config.xml file)
+	private String safeAppID = "";
+
+	// The vault (server) response timeout in seconds to use as specified in the MID config.xml file
+	private String safeTimeout = "";
+
+	private String includeDomain = "";
 
 	// Logger object to log messages in agent.log
 	private static final Logger fLogger = LoggerFactory.getLogger(CredentialResolver.class);
-	
+
 	public CredentialResolver() {
 	}
 
@@ -54,54 +61,56 @@ public class CredentialResolver implements IExternalCredential {
 		return "0.1";
 	}
 
-	
+
 	/**
 	 * Config method with pre-loaded config parameters from config.xml.
+	 *
 	 * @param configMap - contains config parameters with prefix "ext.cred" only.
 	 */
 	@Override
 	public void config(Map<String, String> configMap) {
 		//Note: To load config parameters from MID config.xml if not available in configMap.
 		//propValue = Config.get().getProperty("<Parameter Name>")
-		
+
 		safeAppID = configMap.get(SAFE_USER_APP_ID_PROPERTY);
-		if(isNullOrEmpty(safeAppID)) {
+		if (isNullOrEmpty(safeAppID)) {
 			// use default AppId
 			safeAppID = DEFAULT_SAFE_APP_ID;
 		}
-		
+
 		fLogger.info("SafeAppID: " + safeAppID);
-		
+
 		safeTimeout = configMap.get(SAFE_TIMEOUT_PROPERTY);
-		if(isNullOrEmpty(safeTimeout)) {
+		if (isNullOrEmpty(safeTimeout)) {
 			// use default timeout
 			safeTimeout = DEFAULT_SAFE_TIMEOUT;
 		}
-		
+
 		fLogger.info("safeTimeout: " + safeTimeout);
 
 		includeDomain = configMap.get(CYBERARK_INCLUDE_DOMAIN_PROPERTY);
-		if(isNullOrEmpty(includeDomain)) {
+		if (isNullOrEmpty(includeDomain)) {
 			// include domain for windows username by default.
 			includeDomain = "false";
 		}
-		
+
 		fLogger.info("includeDomain: " + includeDomain);
 
 		safeFolder = configMap.get(SAFE_FOLDER_PROPERTY);
-		
+
 		fLogger.info("safeFolder: " + safeFolder);
-		
-		if(isNullOrEmpty(safeFolder))
+
+		if (isNullOrEmpty(safeFolder)) {
 			fLogger.error("[Vault] INFO - CredentialResolver safeFolder not set!");
+		}
 
 		safeName = configMap.get(SAFE_NAME_PROPERTY);
-		
+
 		fLogger.info("safeName: " + safeName);
-		
-		if(isNullOrEmpty(safeName))
+
+		if (isNullOrEmpty(safeName)) {
 			fLogger.error("[Vault] INFO - CredentialResolver safeSafeName not set!");
-		
+		}
 	}
 
 	/**
@@ -110,15 +119,16 @@ public class CredentialResolver implements IExternalCredential {
 	@Override
 	public Map<String, String> resolve(Map<String, String> args) {
 
-		String credId = (String) args.get(ARG_ID);
-		String credType = (String) args.get(ARG_TYPE);
+		String credId = args.get(ARG_ID);
+		String credType = args.get(ARG_TYPE);
 
 		String username = "";
 		String password = "";
 		String private_key = "";
 
-		if(isNullOrEmpty(credId) || isNullOrEmpty(credType))
+		if (isNullOrEmpty(credId) || isNullOrEmpty(credType)) {
 			throw new RuntimeException("Invalid credential Id or type found.");
+		}
 
 		String policyId = "";
 
@@ -133,7 +143,7 @@ public class CredentialResolver implements IExternalCredential {
 					safeName = parts[0];
 				}
 				credId = parts[1];
-			}  else if (parts.length == 3) {
+			} else if (parts.length == 3) {
 				// Ignore safe name field of credId if empty
 				if (!parts[0].isEmpty()) {
 					safeName = parts[0];
@@ -141,13 +151,13 @@ public class CredentialResolver implements IExternalCredential {
 				credId = parts[1];
 				policyId = parts[2];
 			} else {
-				throw new RuntimeException( "Invalid Credential ID: Credential Id has split string more than twice");
+				throw new RuntimeException("Invalid Credential ID: Credential Id has split string more than twice");
 			}
-			
+
 			fLogger.info("credId: " + credId);
 			fLogger.info("credType: " + credType);
 			fLogger.info("policyId: " + policyId);
-			
+
 			// Connect to vault and retrieve credential
 			PSDKPassword psdkPassword = getCred(safeAppID, credId, safeName, safeFolder, policyId, safeTimeout);
 
@@ -155,57 +165,61 @@ public class CredentialResolver implements IExternalCredential {
 			username = psdkPassword.getUserName();
 			password = psdkPassword.getContent();  // password, private key, etc.
 
-			switch(credType) {
-			// for below listed credential type , just retrieve user name and password 
-			case "windows":
-			case "ssh_password": // Type SSH
-			case "vmware":
-			case "jdbc":
-			case "jms": 
-			case "basic":
-				username = psdkPassword.getUserName();
-				password = psdkPassword.getContent();  // password, private key, etc.
+			switch (credType) {
+				// for below listed credential type , just retrieve user name and password
+				case "windows":
+				case "ssh_password": // Type SSH
+				case "vmware":
+				case "jdbc":
+				case "jms":
+				case "basic":
+					username = psdkPassword.getUserName();
+					password = psdkPassword.getContent();  // password, private key, etc.
 
-				//Optional: for windows/vmware, include domain name
-				if (credType.equals("windows") || credType.equals("vmware")) {
-					// add domain in username if not already exists
-					if (username.indexOf('\\') < 0 && "true".equalsIgnoreCase(includeDomain)) {
-						String domainName = "";
-						// domain is the string in the address field.
-						String address = psdkPassword.getAddress();
-						fLogger.debug("Windows domain name property not found, using address : " + address);
-						if (!isNullOrEmpty(address)) {
-							domainName = address;
-						}
+					//Optional: for windows/vmware, include domain name
+					if (credType.equals("windows") || credType.equals("vmware")) {
+						// add domain in username if not already exists
+						if (username.indexOf('\\') < 0 && "true".equalsIgnoreCase(includeDomain)) {
+							String domainName = "";
+							// domain is the string in the address field.
+							String address = psdkPassword.getAddress();
+							fLogger.debug("Windows domain name property not found, using address : " + address);
+							if (!isNullOrEmpty(address)) {
+								domainName = address;
+							}
 
-						if (!isNullOrEmpty(domainName)) {
-							username = domainName + "\\" + username;
-						} else {
-							// this is required for windows
-							username = ".\\" + username;
+							if (!isNullOrEmpty(domainName)) {
+								username = domainName + "\\" + username;
+							} else {
+								// this is required for windows
+								username = ".\\" + username;
+							}
 						}
 					}
-				}
-				break;
+					break;
 				// for below listed credential type , retrieve user name, password, ssh_passphrase, ssh_private_key
-			case "ssh_private_key": 
-			case "sn_cfg_ansible": 
-			case "sn_disco_certmgmt_certificate_ca":
-			case "cfg_chef_credentials":
-			case "infoblox": 
-			case "api_key":
-				// Read operation
-				username = psdkPassword.getUserName();
-				private_key = psdkPassword.getContent();  // password, private key, etc.
+				case "ssh_private_key":
+				case "sn_cfg_ansible":
+				case "sn_disco_certmgmt_certificate_ca":
+				case "cfg_chef_credentials":
+				case "infoblox":
+				case "api_key":
+					// Read operation
+					username = psdkPassword.getUserName();
+					private_key = psdkPassword.getContent();  // password, private key, etc.
 
-				break;
-			case "aws": ;
-			case "ibm": ; // softlayer_user, softlayer_key, bluemix_key
-			case "azure": ; // tenant_id, client_id, auth_method, secret_key
-			case "gcp": ; // email , secret_key
-			default:
-				fLogger.error("[Vault] INFO - CredentialResolver - invalid credential type found.");
-				break;
+					break;
+				case "aws":
+					;
+				case "ibm":
+					; // softlayer_user, softlayer_key, bluemix_key
+				case "azure":
+					; // tenant_id, client_id, auth_method, secret_key
+				case "gcp":
+					; // email , secret_key
+				default:
+					fLogger.error("[Vault] INFO - CredentialResolver - invalid credential type found.");
+					break;
 			}
 		} catch (Exception e) {
 			// Catch block
@@ -250,13 +264,14 @@ public class CredentialResolver implements IExternalCredential {
 	}
 
 	private static boolean isNullOrEmpty(String str) {
-		if(str != null && !str.trim().isEmpty())
+		if (str != null && !str.trim().isEmpty()) {
 			return false;
+		}
 		return true;
 	}
 
-	//main method to test cyberark on dev machine where cyberark-AIM is installed.
-	//provide cyberark vault details and test it.
+	// main method to test cyberark on dev machine where cyberark-AIM is installed.
+	// provide cyberark vault details and test it.
 	// TODO: Remove this before moving to production
 	public static void main(String[] args) {
 		CredentialResolver credResolver = new CredentialResolver();
@@ -268,8 +283,8 @@ public class CredentialResolver implements IExternalCredential {
 		map.put(ARG_ID, "test-win-credentials");
 		map.put(ARG_TYPE, "windows");
 
-		Map<String, String> result = credResolver.resolve(map );
+		Map<String, String> result = credResolver.resolve(map);
 		fLogger.info(result.toString());
 	}
-	
+
 }
